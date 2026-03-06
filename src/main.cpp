490bd804922a274313e11b7834e4549c80523843
Shiru <8051842+MAJICivil@users.noreply.github.com>
2026-03-06 14:39:50 -0500

    Setup timer1 and added waveform utils file

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
 #include <Arduino.h>
 
+constexpr uint8_t signalOut = 9;
+
+ISR(TIMER1_COMPA_vect) {
+
+}
+
+ISR(TIMER1_OVF_vect) {
+
+}
 
 void setup() {
   Serial.begin(9600);
+
+  TCCR1A = 0;
+  TCCR1B |= (1 << CS10);
 
 }
 
